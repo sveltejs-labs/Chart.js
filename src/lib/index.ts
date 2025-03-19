56e4e10eeaf7e21d85254eba3709c2c9e49e5541
Minsoo Choo <minsoochoo0122@proton.me>
2025-03-19 15:07:02 -0400

    Add packages

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,1 +1,13 @@
 // Reexport your entry components here
+export * from 'chart.js';
+export { default as Chart } from 'chart.js/auto';
+export type * from 'chart.js';
+
+export { default as Bar } from '$lib/components/Bar.svelte';
+export { default as Bubble } from '$lib/components/Bubble.svelte';
+export { default as Doughnut } from '$lib/components/Doughnut.svelte';
+export { default as Line } from '$lib/components/Line.svelte';
+export { default as Pie } from '$lib/components/Pie.svelte';
+export { default as PolarArea } from '$lib/components/PolarArea.svelte';
+export { default as Radar } from '$lib/components/Radar.svelte';
+export { default as Scatter } from '$lib/components/Scatter.svelte';
